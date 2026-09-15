9895d7353d43844b0d61988f9c17d30fafa33757
Alex Sanghez <89218523+Alexsanghez@users.noreply.github.com>
2026-09-15 21:44:07 +0200

    feat: expose enemy randomizer runtime checks

--- client/src/enemy_randomizer_bridge.h
+++ client/src/enemy_randomizer_bridge.h
@@ -10,5 +10,8 @@ enum class EnemyRandomizerPrepareResult {
 };
 
 EnemyRandomizerPrepareResult prepare_enemy_randomizer_config(const std::string& config_text);
+bool enemy_randomizer_runtime_files_available();
+bool enemy_randomizer_ap_is_chain_loaded();
+bool load_enemy_randomizer_runtime_support();
 bool enemy_randomizer_is_available();
 bool launch_enemy_randomizer();
